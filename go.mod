66bae37c3429f07ca5009693d7f901c531398cd7
v.lapin <v.lapin@pikemedia.live>
2022-06-21 19:56:49 +0300

    add report

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/ipinfo/go/v2 v2.7.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -14,7 +15,12 @@ require (
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.13 // indirect
+	github.com/oschwald/geoip2-golang v1.7.0 // indirect
+	github.com/oschwald/maxminddb-golang v1.9.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
+	golang.org/x/sys v0.0.0-20220325203850-36772127a21f // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -3,24 +3,44 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"github.com/jackc/pgx/v4"
-	"io/ioutil"
-	"time"
-
-	//"database/sql"
 	"errors"
 	"fmt"
-	"io"
+	"github.com/ipinfo/go/v2/ipinfo"
+	"github.com/jackc/pgx/v4"
+	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
+type Resolution struct {
+	X int
+	Y int
+}
+
+type Platform struct {
+	Name         string
+	Version      string
+	Architecture string
+}
+
+type BrowserClient struct {
+	Name    string
+	Version string
+}
+
 type BrowserClientInfo struct {
-	UserIP               string `json:"userIP"`
-	Platform             string `json:"platform"`
-	BrowserClient        string `json:"browserClient"`
-	ScreenDataViewPort   string `json:"screenData_viewPort"`
-	ScreenDataResolution string `json:"screenData_resolution"`
+	ScreenDataViewPort   Resolution    `json:"screenData_viewPort"`
+	ScreenDataResolution Resolution    `json:"screenData_resolution"`
+	Platform             Platform      `json:"platform"`
+	BrowserClient        BrowserClient `json:"browserClient"`
+	UserIP               string        `json:"userIP"`
+	UserRegion           string
+	UserProvider         string
 }
 
 type Viewer struct {
@@ -38,11 +58,51 @@ type Viewer struct {
 	ChatCommentsTotal        int32         `json:"chatCommentsTotal"`
 	ChatCommentsDeltaPercent uint8         `json:"chatCommentsDeltaPercent"`
 	AnotherFields            []interface{} `json:"anotherFields"`
-	UserIP                   string        `json:"userIP"`
-	Platform                 string        `json:"platform"`
-	BrowserClient            string        `json:"browserClient"`
-	ScreenDataViewPort       string        `json:"screenData_viewPort"`
-	ScreenDataResolution     string        `json:"screenData_resolution"`
+}
+
+func (resolution *Resolution) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+	split := strings.Split(s, "x")
+	resolution.X, err = strconv.Atoi(split[0])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "UnmarshalJSON failed: %v\n", err)
+		return err
+	}
+	resolution.Y, err = strconv.Atoi(split[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "UnmarshalJSON failed: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func (platform *Platform) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+	split := strings.Split(s, " ")
+	platform.Architecture = split[len(split)-1]
+	platform.Version = split[len(split)-2]
+	platform.Name = s[0 : len(s)-len(platform.Architecture)-len(platform.Version)-2]
+	return nil
+}
+
+func (browserClient *BrowserClient) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+	split := strings.Split(s, " ")
+	browserClient.Version = split[len(split)-1]
+	browserClient.Name = s[0 : len(s)-len(browserClient.Version)-1]
+	return nil
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
@@ -75,7 +135,7 @@ func stat(conn *pgx.Conn, startTime time.Time) func(w http.ResponseWriter, r *ht
 func collect(conn *pgx.Conn) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		sqlStr := `INSERT INTO stats("viewerId","name","lastName","isChatName","email","isChatEmail","joinTime","leaveTime","spentTime","spentTimeDeltaPercent","chatCommentsTotal","chatCommentsDeltaPercent","anotherFields","userIP","platform","browserClient","screenData_viewPort","screenData_resolution") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18)`
+		sqlStr := `INSERT INTO stats("viewerId","name","lastName","isChatName","email","isChatEmail","joinTime","leaveTime","spentTime","spentTimeDeltaPercent","chatCommentsTotal","chatCommentsDeltaPercent","anotherFields","userIP","userRegion","userProvider","platformName","platformVersion","platformArchitecture","browserClientName","browserClientVersion","screenData_viewPortX","screenData_viewPortY","screenData_resolutionX","screenData_resolutionY") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25)`
 		fmt.Printf("got /collect request\n")
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
@@ -98,7 +158,15 @@ func collect(conn *pgx.Conn) func(w http.ResponseWriter, r *http.Request) {
 
 		for _, t := range targets {
 			fmt.Println(t.ViewerId, "-", t.Name)
-			_, err = conn.Exec(context.Background(), sqlStr, t.ViewerId, t.Name, t.LastName, t.IsChatName, t.Email, t.IsChatEmail, t.JoinTime, t.LeaveTime, t.SpentTime, t.SpentTimeDeltaPercent, t.ChatCommentsTotal, t.ChatCommentsDeltaPercent, t.AnotherFields, t.BrowserClientInfo.UserIP, t.BrowserClientInfo.Platform, t.BrowserClientInfo.BrowserClient, t.BrowserClientInfo.ScreenDataViewPort, t.BrowserClientInfo.ScreenDataResolution)
+			client := ipinfo.NewClient(nil, nil, "887d18d82ff5e2")
+			info, err := client.GetIPInfo(net.ParseIP(t.UserIP))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "GetIPInfo failed: %v\n", err)
+			} else {
+				t.UserRegion = info.Region
+				t.UserProvider = info.Org
+			}
+			_, err = conn.Exec(context.Background(), sqlStr, t.ViewerId, t.Name, t.LastName, t.IsChatName, t.Email, t.IsChatEmail, t.JoinTime, t.LeaveTime, t.SpentTime, t.SpentTimeDeltaPercent, t.ChatCommentsTotal, t.ChatCommentsDeltaPercent, t.AnotherFields, t.UserIP, t.UserRegion, t.UserProvider, t.Platform.Name, t.Platform.Version, t.Platform.Architecture, t.BrowserClient.Name, t.BrowserClient.Version /**/, t.ScreenDataViewPort.X, t.ScreenDataViewPort.Y /**/, t.ScreenDataResolution.X, t.ScreenDataResolution.Y)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "collect failed: %v\n", err)
 				os.Exit(1)
@@ -113,9 +181,68 @@ func collect(conn *pgx.Conn) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func report(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /report request\n")
-	io.WriteString(w, "TODO\n")
+func report(conn *pgx.Conn) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("got /report request\n")
+
+		var sqlStr string
+		var rows pgx.Rows
+		var err error
+
+		if r.URL.Query().Has("platformName") {
+			sqlStr = `SELECT "platformVersion", count(*) FROM "stats" WHERE "platformName" = $1 GROUP BY "platformVersion"`
+			rows, err = conn.Query(context.Background(), sqlStr, r.URL.Query().Get("platformName"))
+		} else if r.URL.Query().Has("browserClientName") {
+			sqlStr = `SELECT "browserClientVersion", count(*) FROM "stats" WHERE "browserClientName" = $1 GROUP BY "browserClientVersion"`
+			rows, err = conn.Query(context.Background(), sqlStr, r.URL.Query().Get("browserClientName"))
+		} else if r.URL.Query().Has("column") {
+			switch r.URL.Query().Get("column") {
+			case "platformName":
+				sqlStr = `SELECT "platformName", count(*) FROM "stats" GROUP BY "platformName"`
+			case "browserClientName":
+				sqlStr = `SELECT "browserClientName", count(*) FROM "stats" GROUP BY "browserClientName"`
+			case "browserClient":
+				sqlStr = `SELECT CONCAT("browserClientName", ' ', "browserClientVersion") AS browserClient, count(*) FROM "stats" GROUP BY browserClient`
+			case "screenData_resolution":
+				sqlStr = `SELECT CONCAT("screenData_resolutionX", 'x', "screenData_resolutionY") as screenData_resolution, count(*) FROM "stats" GROUP BY screenData_resolution`
+			case "userRegion":
+				sqlStr = `SELECT "userRegion", count(*) FROM "stats" GROUP BY "userRegion"`
+			case "userProvider":
+				sqlStr = `SELECT "userProvider", count(*) FROM "stats" GROUP BY "userProvider"`
+			}
+			rows, err = conn.Query(context.Background(), sqlStr)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			_, err = fmt.Fprintf(w, "failed")
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "collect failed: %v\n", err)
+				os.Exit(1)
+			}
+			return
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "report failed: %v\n", err)
+			os.Exit(1)
+		}
+		defer rows.Close()
+
+		var name string
+		var cnt int
+
+		fmt.Fprintf(w, "%s,count", r.URL.Query().Get("column"))
+		for rows.Next() {
+			err := rows.Scan(&name, &cnt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = fmt.Fprintf(w, "\n%v,%v", name, cnt)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "report failed: %v\n", err)
+				os.Exit(1)
+			}
+		}
+	}
 }
 
 func main() {
@@ -132,7 +259,7 @@ func main() {
 	mux.HandleFunc("/ping", ping)
 	mux.HandleFunc("/stat", stat(conn, startTime))
 	mux.HandleFunc("/collect", collect(conn))
-	mux.HandleFunc("/report", report)
+	mux.HandleFunc("/report", report(conn))
 
 	err = http.ListenAndServe(":80", mux)
 
